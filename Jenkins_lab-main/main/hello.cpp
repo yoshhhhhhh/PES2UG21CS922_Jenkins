e5ae1444d7b33ac8d3e6a9ce76925746fb0c9096
yoshhhhhhh <yash1999online@gmail.com>
2024-03-11 22:00:30 +0530

    lab5task2

---
+++ Jenkins_lab-main/main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "PES2UG21CS922\n";
+  return 0;
+}
